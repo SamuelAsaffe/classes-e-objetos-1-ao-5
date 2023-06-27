56ced851ac4cfe82098a8ad278972f4494c859cd
SamuelAsaffe <samuelasaffe1@gmail.com>
2023-06-27 15:41:35 -0300

    1 feita

---
+++ tarefa2.js
@@ -0,0 +1,31 @@
+// Exercício 1
+// Crie as seguintes classes com seus respectivos atributos:
+// Cliente(cpf, nome, datanascimento)
+// Produto(codigo, descricao, qtdestoque)
+// Venda(codigo, datahora, cliente, produto)
+
+class Cliente {
+    constructor(cpf, nome, datanascimento) {
+        this.cpf = cpf
+        this.nome = Name
+        this.datanascimento = datanascimento
+    }
+
+}
+class Produto {
+    constructor(codigo, descricao, qtdestoque) {
+        this.codigo = codigo
+        this.descricao = descricao
+        this.qtdestoque = qtdestoque
+    }
+
+}
+class Venda {
+    constructor(codigo, datahora, cliente, produto) {
+        this.codigo = codigo
+        this.datahora = datahora
+        this.cliente = cliente
+        this.produto = produto
+    }
+
+}
